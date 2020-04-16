3b0b59c12cb731862836202a4e1e0e28bee82661
davidgeorgebell <bell.dave93@gmail.com>
2020-04-16 16:00:39 +0800

    save notes to local storage

--- src/components/scramblePad.js
+++ src/components/scramblePad.js
@@ -1,22 +1,31 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 
 const ScramblePad = () => {
-  const [notes, setNotes] = useState("")
+  const [notes, setNotes] = useState([])
 
-  const handleSubmit = e => {
-    e.preventDefault()
-    console.log("submit")
+  useEffect(() => {
+    const notes = localStorage.getItem("scramble-content")
+    if (notes) {
+      setNotes(JSON.parse(notes))
+    }
+  })
+
+  const handleChange = e => {
+    setNotes(e.target.value)
+    localStorage.setItem("scramble-content", JSON.stringify(e.target.value))
   }
+
   return (
-    <form className="scramble__form" onSubmit={handleSubmit}>
-      <label className="scramble__label" for="scramblePad">
+    <form className="scramble__form">
+      <label className="scramble__label" htmlFor="scramblePad">
         <span className="hidden">Add your notes</span>
         <textarea
           className="scramble__input"
           rows="10"
           placeholder="Add notes here 📝"
           name="ScramblePad"
-        />
+          onChange={handleChange}
+        ></textarea>
       </label>
     </form>
   )
